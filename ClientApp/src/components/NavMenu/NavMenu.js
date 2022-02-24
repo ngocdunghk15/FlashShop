73490a6456456a34962bb4a50dcd788ecec78b45
ngocdunghk15 <ngocdunghk15@gmail.com>
2022-02-24 16:27:59 +0700

    Header 90%

---
+++ ClientApp/src/components/NavMenu/NavMenu.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import { Link as LinkS } from 'react-scroll'
+import './NavMenu.css';
+import {
+  LogoBrandWrap,
+  LogoBrand,
+  BrandText,
+  SignInBtn,
+  SignInWrap,
+  SearchWrap,
+  InputSearch,
+  FanpageWrap,
+  FanpageLink
+} from './NavMenuElements';
+
+export class NavMenu extends Component {
+  static displayName = NavMenu.name;
+
+
+  constructor(props) {
+    super(props);
+
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true
+    };
+  }
+
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+
+  render() {
+    return (
+      <header>
+        <Navbar className="navbar-expand-sm navbar-toggleable-sm " light>
+          <Container>
+            <NavbarBrand tag={Link} to="/">
+              <LogoBrandWrap>
+                <LogoBrand src={require('../../images/brand-logo.png').default} />
+                <BrandText>FLSHOP</BrandText>
+              </LogoBrandWrap>
+            </NavbarBrand>
+            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+            <Collapse className="d-sm-inline-flex flex-center" isOpen={!this.state.collapsed} navbar>
+              <ul className="navbar-nav flex-grow navigation">
+                <NavItem className="nav-item">
+                  <NavLink tag={Link} className="text-dark" to="/">About</NavLink>
+                </NavItem>
+                <NavItem className="nav-item">
+                  <NavLink tag={Link} className="text-dark" to="/products">Products</NavLink>
+                </NavItem>
+                <NavItem className="nav-item sub-nav">
+                  <NavLink tag={Link} className="text-dark" to="/products">Services</NavLink>
+                </NavItem >
+                <FanpageWrap className="nav-item" >
+                  <FanpageLink target="_blank" href="https://www.facebook.com/UET-League-Of-Legends-Club-105847131898074" className="text-dark">Fanpage</FanpageLink>
+                </FanpageWrap>
+                <NavItem className="nav-item sub-nav">
+                  <NavLink tag={Link} className="text-dark" to="/products">Help</NavLink>
+                </NavItem>
+                <SignInWrap className="none-list-style">
+                  <SignInBtn className="trim-text" to="/signin">Sign In</SignInBtn>
+                </SignInWrap>
+              </ul>
+            </Collapse>
+            <SearchWrap>
+              <InputSearch placeholder="Search..." />
+            </SearchWrap>
+          </Container>
+        </Navbar>
+      </header>
+    );
+  }
+}
---
+++ ClientApp/src/components/NavMenu/NavMenuElements.js
@@ -0,0 +1,101 @@
+import styled from 'styled-components'
+import { Link as LinkR } from 'react-router-dom'
+export const LogoBrandWrap = styled.div`
+    display:flex;
+    align-items:center;
+    justify-content:space-between;
+    width:164px;
+    @media screen and (max-width:576px){
+        left:50%;
+    }
+`
+
+export const LogoBrand = styled.img`
+    width: 64px;
+    height: 64px;
+`
+
+export const BrandText = styled.h1`
+    color:#F53838 ;
+    font-weight: 700;
+    font-size: 24px;
+    opacity: 0.9;
+    border-radius:4px;
+    padding:2px;
+    letter-spacing:1px;
+    margin:0;
+`
+
+export const SignInBtn = styled(LinkR)`
+    list-style-type: none;
+    text-decoration: none;
+    font-size:16px;
+    display:flex;
+    color:#F53855;
+    font-weight: 700;
+    &:hover{
+        color:#F53855;
+        opacity:0.8;
+    }
+    width:100%;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+    @media screen and (max-width:576px){
+        font-size: 20px;
+        color:#fff;
+        &:hover{
+            color:#fff;
+            opacity:0.8;
+        }
+    }
+`
+
+export const SignInWrap = styled.div`
+    padding: 4px 8px;
+    background-color: #fff;
+    width:86px;
+    height:36px;
+    border:#F53855 solid 1px;
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    border-radius:50px;
+    margin-left: 24px;
+    @media screen and (max-width:768px){
+        margin-left: 0px;
+    }
+    @media screen and (max-width:576px){
+        margin: 12px 0;
+        width: 128px;
+        padding-top:6px;
+        padding-bottom: 6px;
+        background: #F53855;
+    }
+    @media screen and (max-width:768px) and (min-width:576px){
+    }
+`
+
+export const SearchWrap = styled.div`
+    display:flex;
+    margin-left:4px;
+    @media screen and (max-width:992px) {
+        display:none;
+    }
+`
+
+export const InputSearch = styled.input`
+    border-radius:50px;
+    border:#ccc 1px solid;
+    padding:4px 8px;
+    outline:none;
+    color:#747474;
+`
+
+export const FanpageWrap =  styled.div`
+    padding: 7px;
+`
+
+export const FanpageLink = styled.a`
+    text-decoration: none;
+`
